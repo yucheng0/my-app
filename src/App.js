97f063560ae9d7992d33ca00a0863ae4695b54d7
yucheng0 <yucheng0939@gmail.com>
2023-03-07 16:46:43 +0800

    fix: button

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ function App() {
       console.log (error)
     }}
     >
+      {/* 這只是一個按鍵型式, 隨便一個按鍵都可以只要包在LoginFacebook 內即可
+      它用冒泡的方式取得, 不用跳到其它
+      */}
    <FacebookLoginButton/>
+   {/* <button type="button"> hello login</button> */}
 
     </LoginSocialFacebook>
   );
